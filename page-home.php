6b0b21e4d2bf9ab155fa75afd6516c3d17ba8da0
andersonhsilva <anderson.h.silva@gmail.com>
2022-10-06 18:42:02 -0300

    slide

--- page-home.php
+++ page-home.php
@@ -7,24 +7,23 @@ Template Name: Pagina Home
 
 <main>
 
-<section class="d-flex">
+    <section class="d-flex">
         <!-- Início seção home -->
         <div class="container align-self-center pt-5 pb-5">
             <div class="row">
                 <div class="col-md-6 d-flex">
                     <!-- Textos da seção -->
                     <div class="align-self-center">
-                        <h1 class="title">Texto de apoio texto de apoio</h1>
+                        <h1 class="title">Investimentos internacionais seguros, transparentes e lucrativos.</h1>
                         <p class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consectetur, est eu feugiat
-                            ullamcorper, ante eros suscipit elit, volutpat auctor tellus diam in ipsum.
+                            Conheça a FX Green Candle: um novo conceito de corretora. Criada para investidores que precisam tomar decisões de forma descomplicada e não tem tempo a perder.
                         </p>
                         <div class="d-flex justify-content-between">
                             <a href="" class="btn btn-lg btn-success description">
-                                Texto de apoio
+                                Abrir Conta
                             </a>
                             <a href="" class="btn btn-lg btn-outline-success description">
-                                Texto de apoio
+                                Fazer Login
                             </a>
                         </div>
                     </div>
@@ -37,21 +36,21 @@ Template Name: Pagina Home
         </div>
     </section>
 
-<?php
-  if (have_posts()) {
-    while (have_posts()) {
-      the_post();
-  ?>
-      <div class="row mt-5 mb-5">
-        <div class="col">
-          <?php the_content(); ?>
-        </div>
-      </div>
-  <?php
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+    ?>
+            <div class="row mt-5 mb-5">
+                <div class="col">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+    <?php
+        }
+        wp_reset_postdata(); // limpa os dados do laço wp para evitar conflito em algum outro que venha a ser usado
     }
-    wp_reset_postdata(); // limpa os dados do laço wp para evitar conflito em algum outro que venha a ser usado
-  }
-  ?>
+    ?>
 
 </main>
 
